008c9f01ee6e61b66aa7e943f69bfc376afbca3c
coszach@hotmail.it <coszach@hotmail.it>
2022-03-10 14:23:59 +0100

    create work folder

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavComponent />
+    <MainComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavComponent from './components/NavComponent.vue'
+import MainComponent from './components/MainComponent.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavComponent,
+    MainComponent,
   }
 }
 </script>
@@ -21,8 +24,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
